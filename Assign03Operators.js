8c377f0c0d814ebd4492a62b85862028cbefa567
= <rightbil@gmail.com>
2023-06-20 18:57:28 -0400

    Adding Assingment 3

---
+++ Assign03Operators.js
@@ -0,0 +1,24 @@
+let stringType="" 
+let integer=101
+let zero=0
+console.log(stringType+integer+zero) // concat string and all numbers 
+console.log(stringType++) // no number inside the cottation so it is 0
+console.log(integer++) // increment by 1
+let emptyString="" 
+let wholeNumber=-1
+console.log(emptyString+wholeNumber)
+let boolTrue=true 
+let boolFalse=false
+console.log(boolTrue + boolFalse)// check this again
+console.log(6/"3") // 3 is evaluated as number so it is 6/3 = 2
+console.log("2" * "3") // both are numbers so 6 
+console.log(4 + 5 + "px") // will add the nubmers first to 9 then concat to px
+console.log("$" + 4 + 5)
+console.log("4" - 2)
+console.log("4px" - 2) // subtractinon do not work like +
+console.log("  -9  " + 5) // contat as is 
+console.log("  -9  " - 5) // -14 
+console.log(null + 1) // null is as 0 so 1
+console.log(undefined + 1) // undefined with anything is anything
+console.log(" \t \n" - 2) // ask the teacher 
+
